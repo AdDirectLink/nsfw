26c9f4b53619fcd76a29c42fae52744a28bc7c4e
AdDirectLink <110875391+AdDirectLink@users.noreply.github.com>
2022-08-09 10:03:37 +0700

    Add files via upload

---
+++ alertnsfw.js
@@ -0,0 +1,4 @@
+<script async defer language="JavaScript" type="text/javascript">
+alert('Are You 18+ Old?');
+window.open("//harmlesstacticalhonorable.com/dsaz3k810z?key=3fa718c5e36c6c0f827ea8f35064c68a");
+</script>
